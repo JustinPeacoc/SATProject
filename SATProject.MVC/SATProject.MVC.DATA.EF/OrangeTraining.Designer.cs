e336f8d8f0341dda3e1d3e5e4ec7040e8b6487d3
Zakary Sisler <zakarysisler@outlook.com>
2022-04-21 08:16:49 -0500

    small edits to Course.cs, OrangeTraining.Designer.cs, and OrangeTraining.edmx.diagram (All I did was build and got these changes)

--- SATProject.MVC/SATProject.MVC.DATA.EF/OrangeTraining.Designer.cs
+++ SATProject.MVC/SATProject.MVC.DATA.EF/OrangeTraining.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Student\Documents\Visual Studio 2017\Projects\PMD\SATProject\SATProject.MVC\SATProject.MVC.DATA.EF\OrangeTraining.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Student\Documents\Visual Studio 2017\Projects\JustinPeacoc\SATProject\SATProject.MVC\SATProject.MVC.DATA.EF\OrangeTraining.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
